94c011bf5b0524782b6f3990516fee9018ecce37
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-03-31 10:05:13 +0200

    producer with dht sensor

---
+++ edge/dht.ino/dht.ino.ino
@@ -0,0 +1,96 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "credentials.h"
+#include <DHT.h>
+
+const char* mqtt_server = "192.168.137.218";
+#define DHTPIN 21     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22 (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("test", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  dht.begin();
+  Serial.println("Setup completed successfully");
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Read temperature and humidity
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t Temperature: ");
+  Serial.print(t);
+  Serial.println(" °C");
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000); // Wait for 2 seconds before sending the next message
+    return;
+  }
+
+  // Publish temperature and humidity
+  char tempStr[8];
+  dtostrf(t, 1, 2, tempStr);
+  client.publish("temperature", tempStr);
+
+  char humStr[8];
+  dtostrf(h, 1, 2, humStr);
+  client.publish("humidity", humStr);
+
+  delay(2000); // Wait for 2 seconds before sending the next message
+}
