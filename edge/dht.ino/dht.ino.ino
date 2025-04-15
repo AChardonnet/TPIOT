0393a5b2be549750ce80c9691832af74b8b8a150
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-04-15 11:33:59 +0200

    Refactor DHT sensor data publishing to use JSON format for temperature and humidity

--- edge/dht.ino/dht.ino.ino
+++ edge/dht.ino/dht.ino.ino
@@ -1,8 +1,9 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
 #include "credentials.h"
 #include "conf.h"
-#include <DHT.h>
 
 #define DHTPIN 21     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22   // DHT 22 (AM2302)
@@ -40,7 +41,11 @@ void reconnect() {
     if (client.connect("ESP32Client")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("test", "hello world");
+      JsonDocument jsonDocTest;
+      jsonDocTest["test"] = "hello world";
+      char jsonBufferTest[200];
+      serializeJson(jsonDocTest, jsonBufferTest);
+      client.publish("{\"test\": \"string\"}", jsonBufferTest);
       // ... and resubscribe
       client.subscribe("inTopic");
     } else {
@@ -83,14 +88,36 @@ void loop() {
     return;
   }
 
-  // Publish temperature and humidity
-  char tempStr[8];
-  dtostrf(t, 1, 2, tempStr);
-  client.publish("temperature", tempStr);
+  // Publish temperature and humidityJsonDocument jsonDocTemp;
+  JsonDocument jsonDocTemp;
+  jsonDocTemp["schema"] = JsonObject();
+  jsonDocTemp["schema"]["type"] = "struct";
+  jsonDocTemp["schema"]["fields"] = JsonArray();
+  JsonObject tempField = jsonDocTemp["schema"]["fields"].createNestedObject();
+  tempField["type"] = "float";
+  tempField["optional"] = false;
+  tempField["field"] = "temperature";
+  jsonDocTemp["payload"] = JsonObject();
+  jsonDocTemp["payload"]["temperature"] = t;
 
-  char humStr[8];
-  dtostrf(h, 1, 2, humStr);
-  client.publish("humidity", humStr);
+  char jsonBufferTemp[200];
+  serializeJson(jsonDocTemp, jsonBufferTemp);
+  client.publish("{\"temperature\":\"string\"}", jsonBufferTemp);
+
+  JsonDocument jsonDocHumi;
+  jsonDocHumi["schema"] = JsonObject();
+  jsonDocHumi["schema"]["type"] = "struct";
+  jsonDocHumi["schema"]["fields"] = JsonArray();
+  JsonObject humField = jsonDocHumi["schema"]["fields"].createNestedObject();
+  humField["type"] = "float";
+  humField["optional"] = false;
+  humField["field"] = "humidity";
+  jsonDocHumi["payload"] = JsonObject();
+  jsonDocHumi["payload"]["humidity"] = h;
+
+  char jsonBufferHumi[200];
+  serializeJson(jsonDocHumi, jsonBufferHumi);
+  client.publish("{\"humidity\":\"string\"}", jsonBufferHumi);
 
   delay(2000); // Wait for 2 seconds before sending the next message
 }
