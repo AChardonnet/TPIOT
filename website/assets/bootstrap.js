7e677b4fa5a497c61152d709a413199534d57fcf
Antoine CHARDONNET <antoine.chardonnet@centrale-med.fr>
2025-04-24 12:21:19 +0200

    Removed useless js controlers

--- website/assets/bootstrap.js
+++ website/assets/bootstrap.js
@@ -1,9 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-import ChartRefreshController from './controllers/chart_refresh_controller.js';
-import LiveChartController from './controllers/live_chart_controller.js';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-app.register('chart-refresh', ChartRefreshController);
-app.register('live-chart', LiveChartController);
